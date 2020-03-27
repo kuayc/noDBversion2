cf992f6e69b95d8cf8adc48a89cda169612e115f
Chin Kuay <kuaychin@gmail.com>
2020-03-27 15:04:45 -0500

    modified

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const {
+  getItem,
+  postItem,
+  updateItem,
+  removeItem
+} = require("./Controllers/travelController");
 const app = express();
 
 app.use(express.json());
 
-// app.get("/api/travel", getDestinations);
-// app.post("/api/travel", postDestination);
-// app.put("/api/travel/:id", updateDestination);
-// app.delete("/api/travel/:id", removeDestination);
+app.get("/api/todo", getItem);
+app.post("/api/todo", postItem);
+app.put("/api/todo/:id", updateItem);
+app.delete("/api/todo/:id", removeItem);
 
 const PORT = 5000;
-app.listen(PORT, () => console.log(`Big brother listening PORT ${PORT}`));
+
+app.listen(PORT, () => console.log(`Big Brother is running on PORT ${PORT}`));
