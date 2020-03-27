946f6a7655adeb01a7f75b5d043ba85c1666e9f8
Chin Kuay <kuaychin@gmail.com>
2020-03-27 12:06:06 -0500

    modified

--- server/index.js
+++ server/index.js
@@ -1,20 +1,12 @@
 const express = require("express");
-
-const {
-  getDestinations,
-  postDestination,
-  updateDestination,
-  removeDestination
-} = require("./Controllers/travelController");
-
 const app = express();
 
 app.use(express.json());
 
-app.get("/api/travel", getDestinations);
-app.post("/api/travel", postDestination);
-app.put("/api/travel/:id", updateDestination);
-app.delete("/api/travel/:id", removeDestination);
+// app.get("/api/travel", getDestinations);
+// app.post("/api/travel", postDestination);
+// app.put("/api/travel/:id", updateDestination);
+// app.delete("/api/travel/:id", removeDestination);
 
 const PORT = 5000;
 
